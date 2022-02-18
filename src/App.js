429e94cd869145ca451113ac9a13bfd779d35fe7
Kirk Wetherill <kwetherill@digitalpharmacist.com>
2022-02-18 11:45:56 -0600

    Initial files

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@mui/material/Button';
+
+function App() {
+  return <Button variant="contained">Button</Button>;
+}
+
+export default App;
+
