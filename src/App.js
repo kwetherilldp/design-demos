34877f70ca846f0136fb57492169a7c217f89692
Kirk Wetherill <kwetherill@digitalpharmacist.com>
2022-02-22 00:40:38 -0600

    new build

--- src/App.js
+++ src/App.js
@@ -11,8 +11,7 @@ function App() {
         <p>Be passionate about creating beautiful user-freindly products.</p>
       </div>
       <div className="content">
-
-        <a href="https://www.figma.com/proto/cjxsWfFEOYkRFO4g4DUmVA/Onboarding?page-id=0%3A1&node-id=5%3A454&viewport=241%2C48%2C0.59&scaling=scale-down&starting-point-node-id=3%3A4" class="thumbnail">
+        <a href="https://www.figma.com/proto/cjxsWfFEOYkRFO4g4DUmVA/Onboarding-1.0?page-id=0%3A1&node-id=3%3A4&viewport=241%2C48%2C0.47&scaling=scale-down&starting-point-node-id=3%3A4" class="thumbnail">
           <div className="thumbnail-image"></div>
           <div className="thumbnail-text">Schedule Appointment</div>
         </a>
