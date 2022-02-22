1c133d3c4198eaf7997b327f3afa1cb68ad12825
Kirk Wetherill <kwetherill@digitalpharmacist.com>
2022-02-22 00:08:33 -0600

    Change thumbnail image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import * as React from 'react';
-import ReactDOM from 'react-dom';
-import Button from '@mui/material/Button';
+// import ReactDOM from 'react-dom';
+// import Button from '@mui/material/Button';
 
 function App() {
-  return <Button variant="contained">Button</Button>;
+  return (
+    <div className="container">
+      <div className="banner">
+        <h2>Digital Pharmacist</h2>
+        <h1>UX Demos</h1>
+        <p>Be passionate about creating beautiful user-freindly products.</p>
+      </div>
+      <div className="content">
+
+        <a href="https://www.figma.com/proto/cjxsWfFEOYkRFO4g4DUmVA/Onboarding?page-id=0%3A1&node-id=5%3A454&viewport=241%2C48%2C0.59&scaling=scale-down&starting-point-node-id=3%3A4" class="thumbnail">
+          <div className="thumbnail-image"></div>
+          <div className="thumbnail-text">Schedule Appointment</div>
+        </a>
+        
+      </div>
+    </div>
+  );
 }
 
 export default App;
